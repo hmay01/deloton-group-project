cf14364fb7286c5424dfc25beaabc891567d9d62
Yusra Anshoor <yusraanshoor85@gmail.com>
2022-10-05 19:12:39 +0100

    adding skeleton  functions for endpoints

--- app.py
+++ app.py
@@ -30,3 +30,27 @@ def get_daily_rides():
     daily_rides_dict = json.loads(daily_rides_json_string)
     daily_rides_json = json.dumps(daily_rides_dict, indent=4) 
     return   daily_rides_json
+
+@app.route('/ride/<id>', methods=['GET','DELETE'])
+def ride_id(id):
+    if (request.method == 'GET'):
+        #Get a ride with a specific ID
+        return
+    elif (request.method == 'DELETE'):
+        #Delete a with a specific ID
+        return
+
+@app.route('/rider/<user_id>', methods=['GET'])
+def get_rider_info(user_id):
+    #Get rider information (e.g. name, gender, age, avg. heart rate, number of rides)
+    return
+
+@app.route('/rider/<user_id>/rides', methods=['GET'])
+def get_all_rides_for_given_user(user_id):
+    #Get all rides for a rider with a specific ID
+    return
+
+@app.route('/rider/<user_id>/rides', methods=['GET'])
+def get_all_rides_for_given_user(user_id):
+    #Get all rides for a rider with a specific ID
+    return

98baff6cf555a30c05ed3f95b6ba3031152d8653
Yusra Anshoor <yusraanshoor85@gmail.com>
2022-10-05 17:48:32 +0100

    removing sqlalchemy alternative code and .env variables

--- app.py
+++ app.py
@@ -3,34 +3,20 @@
 # %pip install pyarrow
 
 from datetime import datetime
-from os import getenv
-from snowflake_helpers import connect_to_snowflake
+# from os import getenv
+from snowflake_helpers import *
+import json
 
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
-# from flask_cors import CORS
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy import ForeignKey, case, desc, func
-
-load_dotenv()
-
-SNOW_USER = getenv('SNOW_USER')
-ACCOUNT = getenv('ACCOUNT')
-PASSWORD = getenv('PASSWORD')
-WAREHOUSE = getenv('WAREHOUSE')
-DATABASE = getenv('DATABASE')
-SIGMA_SCHEMA = getenv('SIGMA_SCHEMA')
-STAGING_SCHEMA = getenv('STAGING_SCHEMA')
+import snowflake.connector
 
 conn = connect_to_snowflake()
 cs = conn.cursor()
+use_staging_schema(cs)
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = f"snowflake://{SNOW_USER}:{PASSWORD}@{ACCOUNT}/{DATABASE}/{SIGMA_SCHEMA}?warehouse={WAREHOUSE}"
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# CORS(app, origins=["http://127.0.0.1:8080"],  supports_credentials=True)
-# db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -39,4 +25,8 @@ app.run()
 
 @app.route('/daily', methods=['GET'])
 def get_daily_rides():
-    daily_rides = cs.execute("SELECT * FROM")
+    daily_rides_df = cs.execute("SELECT * FROM DF_TEST_TABLE").fetch_pandas_all()
+    daily_rides_json = daily_rides_df.to_json(orient="index")
+    parsed = json.loads(daily_rides_json)
+   
+    return  json.dumps(parsed, indent=4)  

48ed842c8321383123ef8e9919bb5efb5e8b0ee3
Yusra Anshoor <yusraanshoor85@gmail.com>
2022-10-05 16:14:07 +0100

    transfering code to python file

--- backend_api/delton_api.py
+++ backend_api/delton_api.py
@@ -10,3 +10,24 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey, case, desc, func
+
+load_dotenv()
+
+SNOW_USER = getenv('SNOW_USER')
+ACCOUNT = getenv('ACCOUNT')
+PASSWORD = getenv('PASSWORD')
+WAREHOUSE = getenv('WAREHOUSE')
+DATABASE = getenv('DATABASE')
+SIGMA_SCHEMA = getenv('SIGMA_SCHEMA')
+STAGING_SCHEMA = getenv('STAGING_SCHEMA')
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"snowflake://{SNOW_USER}:{PASSWORD}@{ACCOUNT}/{DATABASE}/{SIGMA_SCHEMA}?warehouse={WAREHOUSE}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# CORS(app, origins=["http://127.0.0.1:8080"],  supports_credentials=True)
+db = SQLAlchemy(app)
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Welcome to the Social News API!"

9f8f0984c665deb07c0358564dc3546445588e98
Yusra Anshoor <yusraanshoor85@gmail.com>
2022-10-05 17:55:52 +0100

    renaming variables

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app.run()
 @app.route('/daily', methods=['GET'])
 def get_daily_rides():
     daily_rides_df = cs.execute("SELECT * FROM DF_TEST_TABLE").fetch_pandas_all()
-    daily_rides_json = daily_rides_df.to_json(orient="index")
-    parsed = json.loads(daily_rides_json)
-   
-    return  json.dumps(parsed, indent=4)  
+    daily_rides_json_string = daily_rides_df.to_json(orient="index")
+    daily_rides_dict = json.loads(daily_rides_json_string)
+    daily_rides_json = json.dumps(daily_rides_dict, indent=4) 
+    return   daily_rides_json

9bb6bb94e40fdb529282e6fa32cc7d9dd1d25a0f
hmay01 <harryjmay1@gmail.com>
2022-10-06 08:37:33 +0100

    move staging into python script

---
+++ staging.py
@@ -0,0 +1,20 @@
+from kafka_helpers import (KAFKA_TOPIC_NAME, connect_to_kafka_consumer,
+                           stream_kafka_topic)
+from snowflake_helpers import (connect_to_snowflake, create_logs_table,
+                               create_staging_schema, use_staging_schema)
+
+
+if __name__ == "__main__":
+    conn = connect_to_snowflake()
+    cs = conn.cursor()
+
+    create_staging_schema(cs)
+    use_staging_schema(cs)
+
+    create_logs_table(cs)
+
+    consumer = connect_to_kafka_consumer()
+    stream_kafka_topic(consumer, KAFKA_TOPIC_NAME, cs)
+
+
+

c4da7722912886ad3441e88a7ceb296ff1d00814
Yusra Anshoor <yusraanshoor85@gmail.com>
2022-10-08 15:11:55 +0100

    stopping modification tracking

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ cs.execute(f"USE SCHEMA {PRODUCTION_SCHEMA}")
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.route('/', methods=['GET'])
 def index() -> str:

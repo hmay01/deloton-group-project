4b9d8943101e22447e3908312c86517934328826
Yusra Anshoor <yusraanshoor85@gmail.com>
2022-10-08 15:14:52 +0100

    initialising db

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
 @app.route('/', methods=['GET'])
 def index() -> str:
     return "Welcome to the Deloton Exercise Bikes API!"

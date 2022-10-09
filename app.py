f09a125f02796146e430f8858ed26099172435f1
Yusra Anshoor <yusraanshoor85@gmail.com>
2022-10-09 19:01:26 +0100

    adding session commit to delete endpoint

--- app.py
+++ app.py
@@ -159,6 +159,7 @@ def delete_by_id(id:int) -> str:
     Deletes a ride with a specific ID
     """
     db.session.execute(f'DELETE FROM yusra_stories_production.rides WHERE ride_id = {id};')
+    db.session.commit()
   
     return 'Ride Deleted!', 200
 

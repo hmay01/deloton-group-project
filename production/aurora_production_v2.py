b423116b78fb2acbfbcf6afcf96e9a1e2c3265c2
hmay01 <harryjmay1@gmail.com>
2022-10-11 17:59:47 +0100

    production post test

--- production/aurora_production_v2.py
+++ production/aurora_production_v2.py
@@ -1,40 +1,34 @@
 
 import warnings
 
-from pandas.core.common import SettingWithCopyWarning
-
-from staging.aurora_postgres_helpers import (SQLConnection, get_latest_ride_logs,
-                                     user_already_in_table)
-from production.transformation_helpers import *
-
 #insignificant warnings filtered as they hide important print messages
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action='ignore', category=SyntaxWarning)
-warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
+
+from production_helpers import SQLConnection as sql
+from production_helpers import Transform as t
 
 if __name__ == "__main__":
-
-    sql = SQLConnection
     staging_schema = 'yusra_stories_staging'
     production_schema = 'yusra_stories_production'
     logs_table = 'logs'
 
     #general transformations
-    latest_logs = get_latest_ride_logs(sql)
-    latest_formatted = get_joined_formatted_df(latest_logs)
+    latest_logs = sql.get_latest_ride_logs()
+    latest_formatted = t.get_joined_formatted_df(latest_logs)
 
     #rides table
-    staging_ride_df = get_staging_rides_df(latest_formatted)
-    latest_ride_df = get_final_rides_df(staging_ride_df)
-    sql.write_df_to_table(sql, latest_ride_df, production_schema, 'rides', 'append')
+    staging_ride_df = t.get_staging_rides_df(latest_formatted)
+    latest_ride_df = t.get_final_rides_df(staging_ride_df)
+    sql.write_df_to_table(latest_ride_df, production_schema, 'rides', 'append')
 
     #users table
-    latest_user_df = get_users_df(latest_formatted)
+    latest_user_df = t.get_users_df(latest_formatted)
     latest_user_id = latest_user_df["user_id"].iloc[0]
 
-    if user_already_in_table(sql, latest_user_id):
+    if sql.user_already_in_table(latest_user_id):
         print(f'user_id: {latest_user_id} ALREADY IN USERS TABLE')
     else:
-        sql.write_df_to_table(sql, latest_user_df, production_schema, 'users', 'append')
+        sql.write_df_to_table(latest_user_df, production_schema, 'users', 'append')
 
 

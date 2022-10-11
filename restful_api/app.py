fbe32753531fd1207e4d52230b1620b6f7f29ccf
Yusra Anshoor <yusraanshoor85@gmail.com>
2022-10-11 17:59:38 +0100

    adding helper functions to classes and using classes in script

--- restful_api/app.py
+++ restful_api/app.py
@@ -1,13 +1,13 @@
 
-from app_helpers import *
+from app_helpers import Functionality, json, request
 
 
-@app.route('/', methods=['GET'])
+@Functionality.app.route('/', methods=['GET'])
 def index() -> str:
     return "Welcome to the Deloton Exercise Bikes API!"
 
 
-@app.route('/daily', methods=['GET'])
+@Functionality.app.route('/daily', methods=['GET'])
 def get_rides() -> json:
     """
     Returns a JSON object of all rides occurring on the date specified
@@ -17,37 +17,37 @@ def get_rides() -> json:
     searched_date = request.args.get('date')
     if searched_date == None:
 
-        return get_todays_rides()
+        return Functionality.get_todays_rides()
     else:
        
-        return get_rides_at_specific_date(searched_date)
+        return Functionality.get_rides_at_specific_date(searched_date)
 
-@app.route('/ride/<id>', methods=['GET','DELETE'])
+@Functionality.app.route('/ride/<id>', methods=['GET','DELETE'])
 def ride_id(id:int) -> json:
     """
     For a given ID string input, returns a different JSON object
     based on the chosen request method
     """
     if (request.method == 'GET'):
-        return get_ride_by_id(id)
+        return Functionality.get_ride_by_id(id)
 
     if (request.method == 'DELETE'):
 
-        return delete_by_id(id)
+        return Functionality.delete_by_id(id)
 
-@app.route('/rider/<user_id>', methods=['GET'])
+@Functionality.app.route('/rider/<user_id>', methods=['GET'])
 def get_rider_info(user_id:int) -> json:
     """
     Returns a JSON object containing rider information (e.g. name, gender, age, 
     avg. heart rate, number of rides) for a rider with a specific ID string input
     """
-    return get_rider_info_by_id(user_id)
+    return Functionality.get_rider_info_by_id(user_id)
 
-@app.route('/rider/<user_id>/rides', methods=['GET'])
+@Functionality.app.route('/rider/<user_id>/rides', methods=['GET'])
 def get_all_rides_for_given_user(user_id:int) -> json:
     """
     Returns a JSON object containing all rides for a rider with 
     a specific ID string input
     """
-    return get_all_rides_for_rider(user_id)
+    return Functionality.get_all_rides_for_rider(user_id)
 
